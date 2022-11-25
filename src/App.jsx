a41b064937e24f27e90324491add990c6c66455a
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-25 22:09:49 +0900

    :bookmark: update: レンダリングの処理を記述
    レンダリングの最適化を学ぶ処理を記述しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import { useState, useCallback, useMemo } from 'react';
+import { ChildArea } from './components/ChildArea';
 
-function App() {}
+const App = () => {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = e => {
+    setText(e.target.value);
+  };
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const temp = useMemo(() => {
+    1 + 3;
+  }, []);
+  console.log(temp);
+
+  return (
+    <div className='App'>
+      <input type='text' value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={toggleOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+};
 
 export default App;
